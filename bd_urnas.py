089e4d17885c02cf2e0d79f5d4033fbe4935b79e
John Robson <John.Robson@USP.br>
2022-11-20 15:47:11 -0500

    bd_urnas atualizado e inserido novos arquivos.

--- bd_urnas.py
+++ bd_urnas.py
@@ -33,93 +33,101 @@ Session = scoped_session(sessionmaker(autoflush=True, autocommit=False, bind=eng
 
 
 class Urnas(Base):
-  r''' Esta tabela é Exclusiva para o 2o Turno,
-  com 2 cargos de Presidente e Governador,
-  para o 1o Turno é necessário criar uma tabela de candidatos. '''
-  __tablename__ = "Urnas"
+	r''' Esta tabela é Exclusiva para o 2o Turno,
+	com 2 cargos de Presidente e Governador,
+	para o 1o Turno é necessário criar uma tabela de candidatos. '''
+	__tablename__ = "Urnas"
 
-  id = Column(Integer, primary_key=True)
-  turno = Column(SmallInteger, default=2)
+	id = Column(Integer, primary_key=True)
+	turno = Column(SmallInteger, default=2)
 
-  # Dados do Arquivo BU da Urna
+	# Dados do Arquivo BU da Urna
 
-  # Localização da Urna
-  regiao = Column(String(2), nullable=False)
-  estado = Column(String(2), nullable=False)
-  municipio = Column(SmallInteger, nullable=False)
-  zona = Column(SmallInteger, nullable=False)
-  local = Column(SmallInteger, nullable=False)
-  secao = Column(SmallInteger, nullable=False)
+	# Localização da Urna
+	regiao = Column(String(2), nullable=False)
+	estado = Column(String(2), nullable=False)
+	municipio = Column(SmallInteger, nullable=False)
+	zona = Column(SmallInteger, nullable=False)
+	local = Column(SmallInteger, nullable=False)
+	secao = Column(SmallInteger, nullable=False)
 
-  # Registro dos Votos na Urna Para Presidente, Governador, Brancos e Nulos
-  vPresA22 = Column(SmallInteger, default=0) # Votos Bolsonaro
-  vPresB13 = Column(SmallInteger, default=0) # Votos Lula
-  vPresNulo = Column(SmallInteger, default=0)
-  vPresBranco = Column(SmallInteger, default=0)
+	# Registro dos Votos na Urna Para Presidente, Governador, Brancos e Nulos
+	vPresA22 = Column(SmallInteger, default=0) # Votos Bolsonaro
+	vPresB13 = Column(SmallInteger, default=0) # Votos Lula
+	vPresNulo = Column(SmallInteger, default=0)
+	vPresBranco = Column(SmallInteger, default=0)
 
-  vGovA = Column(SmallInteger) # Mais Alinhado com Bolsonaro
-  vGovB = Column(SmallInteger) # Mais Alinhado com Lula
-  vGovNulo = Column(SmallInteger)
-  vGovBranco = Column(SmallInteger)
+	vGovA = Column(SmallInteger) # Mais Alinhado com Bolsonaro
+	vGovB = Column(SmallInteger) # Mais Alinhado com Lula
+	vGovNulo = Column(SmallInteger)
+	vGovBranco = Column(SmallInteger)
 
-  qEleitAptos = Column(SmallInteger, nullable=False)
-  qComparecimento = Column(SmallInteger, default=0)
-  qEleitLibCodigo = Column(SmallInteger, default=0)
-  qEleitBiometrico = Column(SmallInteger, default=0)
+	qEleitAptos = Column(SmallInteger, nullable=False)
+	qComparecimento = Column(SmallInteger, default=0)
+	qEleitLibCodigo = Column(SmallInteger, default=0)
+	qEleitBiometrico = Column(SmallInteger, default=0)
 
-  # Dados do Arquivo de LOG da Urna # não usar "nullable=False"
-  vPresTotal = Column(SmallInteger) # conta os votos de presidente recebidos no log da urna, tem que bater com o BU
-  modelUrna = Column(String(6), default='UE0000') # 'UE0000' sem arquivo de log | 'UE1111' com arquivo de log, mas sem informar o modelo da urna
+	# Dados do Arquivo de LOG da Urna # não usar "nullable=False"
+	vPresTotal = Column(SmallInteger) # conta os votos de presidente recebidos no log da urna, tem que bater com o BU
+	modelUrna = Column(String(6), default='UE0000') # 'UE0000' sem arquivo de log | 'UE1111' com arquivo de log, mas sem informar o modelo da urna
 
-  # tipoUrna = Column(String(12), nullable=False)
-  # versaoVotacao = Column(String, nullable=False)
+	# Estudos
+	jaVotou = Column(SmallInteger) # quando o mesário tenta habilitar alguém que já tinha vitado (provavelmente o mesário votou no lugar daquela pessoa e mais tarde ela apareceu para votar)
+	vPresIntervalo = Column(String) # 100 menores intervalos de tempo em segundos entre 2 votos durante a votação
+	vConfPres1min = Column(String) # total votos confirmados para Presidente a cada 1 minuto
 
-  # Estudos
-  jaVotou = Column(SmallInteger) # quando o mesário tenta habilitar alguém que já tinha vitado (provavelmente o mesário votou no lugar daquela pessoa e mais tarde ela apareceu para votar)
-  vPresIntervalo = Column(String) # 100 menores intervalos de tempo em segundos entre 2 votos durante a votação
-  vConfPres1min = Column(String) # total votos confirmados para Presidente a cada 1 minuto
+	# Mais Dados Gerais da Urna (Arquivo BU)
+	arquivoBU = Column(String(23), nullable=False)
+	# idEleicao = Column(SmallInteger, nullable=False)
+	dataHoraAbertura = Column(DateTime)
+	dataHoraEncerramento = Column(DateTime)
+	# dataGeracao = Column(DateTime, nullable=False)
+	# dataHoraEmissao = Column(DateTime, nullable=False)
+	# codigoCarga1 = Column(String(24), nullable=False)
+	# dataHoraCarga1 = Column(DateTime, nullable=False)
+	numeroInternoUrna1 = Column(Integer, nullable=False)
+	numeroSerieFC1 = Column(String(8), nullable=False)
+	# codigoCarga2 = Column(String(24), nullable=False)
+	# dataHoraCarga2 = Column(DateTime, nullable=False)
+	numeroInternoUrna2 = Column(Integer, nullable=False)
+	numeroSerieFC2 = Column(String(8), nullable=False)
+	numeroSerieFV = Column(String(8), nullable=False)
 
-  # Mais Dados Gerais da Urna (Arquivo BU)
-  arquivoBU = Column(String(23), nullable=False)
-  # idEleicao = Column(SmallInteger, nullable=False)
-  dataHoraAbertura = Column(DateTime)
-  dataHoraEncerramento = Column(DateTime)
-  # dataHoraEmissao = Column(DateTime, nullable=False)
-  # dataGeracao = Column(DateTime, nullable=False)
-  # codigoCarga = Column(String(24), nullable=False)
-  # dataHoraCarga = Column(DateTime, nullable=False)
-  # numeroInternoUrna = Column(Integer, nullable=False)
-  # numeroSerieFC = Column(String(8), nullable=False)
-  # numeroSerieFV = Column(String(8), nullable=False)
+	numeroInternoUrna2 = Column(Integer, nullable=False)
+	numeroSerieFC2 = Column(String(8), nullable=False)
+	numeroSerieFV = Column(String(8), nullable=False)
 
-  # Colunas Extras para uso Opcional e Testes
-  '''
-  int1 = Column(Integer)
-  int2 = Column(Integer)
-  int3 = Column(Integer)
+	# tipoUrna = Column(String(12), nullable=False)
+	# versaoVotacao = Column(String, nullable=False)
 
-  flt1 = Column(Float)
-  flt2 = Column(Float)
-  flt3 = Column(Float)
-  flt4 = Column(Float)
-  flt5 = Column(Float)
+	# Colunas Extras para uso Opcional e Testes
+	'''
+	int1 = Column(Integer)
+	int2 = Column(Integer)
+	int3 = Column(Integer)
 
-  txt1 = Column(String)
-  txt2 = Column(String)
-  '''
+	flt1 = Column(Float)
+	flt2 = Column(Float)
+	flt3 = Column(Float)
+	flt4 = Column(Float)
+	flt5 = Column(Float)
+
+	txt1 = Column(String)
+	txt2 = Column(String)
+	'''
 
 
 def add_column(engine, table_name, column):
-  column_name = column.compile(dialect=engine.dialect)
-  column_type = column.type.compile(engine.dialect)
-  engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
+	column_name = column.compile(dialect=engine.dialect)
+	column_type = column.type.compile(engine.dialect)
+	engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
 
 
 if __name__ == '__main__':
-  print('Banco de Dados')
+	print('Banco de Dados')
 
-  # insere coluna no Banco de Dados
-  column = Column('fastVotes3', SmallInteger, default=0)
-  # column = Column('fastVotes2', SmallInteger, nullable=False)
+	# insere coluna no Banco de Dados
+	column = Column('fastVotes3', SmallInteger, default=0)
+	# column = Column('fastVotes2', SmallInteger, nullable=False)
 
-  add_column(engine, 'Urnas', column)
+	add_column(engine, 'Urnas', column)
--- src_config.py
+++ src_config.py
@@ -38,33 +38,33 @@ turnos = {1: ('2022-10-01', '2022-10-02'), 2: ('2022-10-28', '2022-10-30')}
 
 
 federacao = {'DF': ['CO', 'Distrito Federal', 15, 43], # Ibaneis Rocha (15 MDB) X (43 PV) Leandro Grass
-            'GO': ['CO', 'Goiás', 44, 22], # Ronaldo Caiado (União Brasil) X (22 PL) Major Vitor Hugo
-            'MT': ['CO', 'Mato Grosso', 44, 43], # Mauro Mendes (44 União Brasil) X (43 PV) Márcia Pinheiro
-            'MS': ['CO', 'Mato Grosso do Sul', 28, 45], # Capitão Contar (28 PRTB) X Eduardo Riedel (45 PSDB)
-            'AL': ['NE', 'Alagoas', 44, 15], # Rodrigo Cunha (44 União Brasil) X Paulo Dantas (15 MDB)
-            'BA': ['NE', 'Bahia', 44, 13], # ACM Neto (neutro) (44 União Brasil) X Jerônimo Rodrigues (13 PT)
-            'CE': ['NE', 'Ceará', 44, 13], # Capitão Wagner (44 União Brasil) X Elmano de Freitas (13 PT)
-            'MA': ['NE', 'Maranhão', 20, 40], # Lahesio Bonfim (20 PSC)X Carlos Brandão (40 PSB)
-            'PB': ['NE', 'Paraíba', 45, 40], # Pedro Cunha Lima (neutro) (45 PSDB) X João Azevêdo (40 PSB)
-            'PE': ['NE', 'Pernambuco', 45, 77], # Raquel Lyra (45 PSDB) X Marília Arraes (77 Solidariedade)
-            'PI': ['NE', 'Piauí', 44, 13], # Silvio Mendes (44 União Brasil) X Rafael Fonteles (13 PT)
-            'RN': ['NE', 'Rio Grande do Norte', 77, 13], # Fabio Dantas (77 Solidariedade) X Fátima Bezerra (13 PT)
-            'SE': ['NE', 'Sergipe', 55, 13], # Fábio Mitidieri (55 PSD) X Rogério Carvalho (13 PT)
-            'AC': ['N', 'Acre', 11, 13], # Gladson Cameli (11 PP) x Jorge Viana (13 PT)
-            'AP': ['N', 'Amapá', 55, 77], # Jaime Nunes (55 PSD) x Clécio (77 SD)
-            'AM': ['N', 'Amazonas', 44, 15], # Wilson Lima (44 União Brasil) X Eduardo Braga (15 MDB)
-            'PA': ['N', 'Pará', 22, 15], # Zequina Marinho (22 PL) X Helder Barbalho (15 MDB)
-            'RO': ['N', 'Rondônia', 22, 44], # Marcos Rogério (22 PL) X Coronel Marcos Rocha (44 União Brasil)
-            'RR': ['N', 'Roraima', 11, 15], # Antônio Denarium (11 PP) X Tereza Surita (15 MDB)
-            'TO': ['N', 'Tocantins', 10, 22], # Wanderlei Barbosa (10 Republicanos) X Ronaldo Dimas (22 PL)
-            'ES': ['SE', 'Espírito Santo', 22, 40], # Carlos Manato (22 PL) X Renato Casagrande (40 PSB)
-            'MG': ['SE', 'Minas Gerais', 30, 13], # Romeu Zema (Novo) X Kalil (13 PT)
-            'RJ': ['SE', 'Rio de Janeiro', 22, 40], # Cláudio Castro (PL) X Marcelo Freixo (PSB 40)
-            'SP': ['SE', 'São Paulo', 10, 13], # Tarcísio de Freitas (10 Republicanos) X Fernando Haddad (13 PT)
-            'PR': ['S', 'Paraná', 55, 13], # Ratinho Jr. (55 PSD) X Requião (13 PT)
-            'RS': ['S', 'Rio Grande do Sul', 22, 45], # Onyx Lorenzoni (22 PL) X Eduardo Leite (neutro) (45 PSDB)
-            'SC': ['S', 'Santa Catarina', 22, 13], # Jorginho Mello (22 PL) X Décio Lima (13 PT)
-            'ZZ': ['EX', 'Exterior', 0, 0]}
+						'GO': ['CO', 'Goiás', 44, 22], # Ronaldo Caiado (União Brasil) X (22 PL) Major Vitor Hugo
+						'MT': ['CO', 'Mato Grosso', 44, 43], # Mauro Mendes (44 União Brasil) X (43 PV) Márcia Pinheiro
+						'MS': ['CO', 'Mato Grosso do Sul', 28, 45], # Capitão Contar (28 PRTB) X Eduardo Riedel (45 PSDB)
+						'AL': ['NE', 'Alagoas', 44, 15], # Rodrigo Cunha (44 União Brasil) X Paulo Dantas (15 MDB)
+						'BA': ['NE', 'Bahia', 44, 13], # ACM Neto (neutro) (44 União Brasil) X Jerônimo Rodrigues (13 PT)
+						'CE': ['NE', 'Ceará', 44, 13], # Capitão Wagner (44 União Brasil) X Elmano de Freitas (13 PT)
+						'MA': ['NE', 'Maranhão', 20, 40], # Lahesio Bonfim (20 PSC)X Carlos Brandão (40 PSB)
+						'PB': ['NE', 'Paraíba', 45, 40], # Pedro Cunha Lima (neutro) (45 PSDB) X João Azevêdo (40 PSB)
+						'PE': ['NE', 'Pernambuco', 45, 77], # Raquel Lyra (45 PSDB) X Marília Arraes (77 Solidariedade)
+						'PI': ['NE', 'Piauí', 44, 13], # Silvio Mendes (44 União Brasil) X Rafael Fonteles (13 PT)
+						'RN': ['NE', 'Rio Grande do Norte', 77, 13], # Fabio Dantas (77 Solidariedade) X Fátima Bezerra (13 PT)
+						'SE': ['NE', 'Sergipe', 55, 13], # Fábio Mitidieri (55 PSD) X Rogério Carvalho (13 PT)
+						'AC': ['N', 'Acre', 11, 13], # Gladson Cameli (11 PP) x Jorge Viana (13 PT)
+						'AP': ['N', 'Amapá', 55, 77], # Jaime Nunes (55 PSD) x Clécio (77 SD)
+						'AM': ['N', 'Amazonas', 44, 15], # Wilson Lima (44 União Brasil) X Eduardo Braga (15 MDB)
+						'PA': ['N', 'Pará', 22, 15], # Zequina Marinho (22 PL) X Helder Barbalho (15 MDB)
+						'RO': ['N', 'Rondônia', 22, 44], # Marcos Rogério (22 PL) X Coronel Marcos Rocha (44 União Brasil)
+						'RR': ['N', 'Roraima', 11, 15], # Antônio Denarium (11 PP) X Tereza Surita (15 MDB)
+						'TO': ['N', 'Tocantins', 10, 22], # Wanderlei Barbosa (10 Republicanos) X Ronaldo Dimas (22 PL)
+						'ES': ['SE', 'Espírito Santo', 22, 40], # Carlos Manato (22 PL) X Renato Casagrande (40 PSB)
+						'MG': ['SE', 'Minas Gerais', 30, 13], # Romeu Zema (Novo) X Kalil (13 PT)
+						'RJ': ['SE', 'Rio de Janeiro', 22, 40], # Cláudio Castro (PL) X Marcelo Freixo (PSB 40)
+						'SP': ['SE', 'São Paulo', 10, 13], # Tarcísio de Freitas (10 Republicanos) X Fernando Haddad (13 PT)
+						'PR': ['S', 'Paraná', 55, 13], # Ratinho Jr. (55 PSD) X Requião (13 PT)
+						'RS': ['S', 'Rio Grande do Sul', 22, 45], # Onyx Lorenzoni (22 PL) X Eduardo Leite (neutro) (45 PSDB)
+						'SC': ['S', 'Santa Catarina', 22, 13], # Jorginho Mello (22 PL) X Décio Lima (13 PT)
+						'ZZ': ['EX', 'Exterior', 0, 0]}
 
 estados_federacao = federacao.keys() # lista com os nomes dos estados
 
@@ -72,17 +72,17 @@ estados_federacao = federacao.keys() # lista com os nomes dos estados
 
 urna_log_remove_texto_exato = None # filtrar textos do Log da Urna
 with open('urna_log_remove_texto_exato.txt', 'r') as text_remove:
-  urna_log_remove_texto_exato = text_remove.read().splitlines() # readlines()
+	urna_log_remove_texto_exato = text_remove.read().splitlines() # readlines()
 
 urna_log_remove_texto_contendo = None # filtrar textos do Log da Urna
 with open('urna_log_remove_texto_contendo.txt', 'r') as text_remove:
-  urna_log_remove_texto_contendo = text_remove.read().splitlines() # readlines()
+	urna_log_remove_texto_contendo = text_remove.read().splitlines() # readlines()
 
 dtf_bu = '%Y%m%dT%H%M%S' # formato para conversão "ISO 8601 Short" no BU
 
 
 def dtf_log(s): # formato para conversão das datas no arquivo log
-  return dt.datetime.strptime(s, '%d/%m/%Y %H:%M:%S')
+	return dt.datetime.strptime(s, '%d/%m/%Y %H:%M:%S')
 
 
 warnings.filterwarnings('ignore', category=FutureWarning, message=('.*will attempt to set the values inplace instead of always setting a new array. To retain the old behavior, use either.*'), )
@@ -90,5 +90,5 @@ warnings.filterwarnings('ignore', category=FutureWarning, message=('.*will attem
 
 
 if __name__ == '__main__':
-  print('Variáveis Utilizadas')
-  print(federacao)
+	print('Variáveis Utilizadas')
+	print(federacao)
